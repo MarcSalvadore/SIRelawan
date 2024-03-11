6506476b23556acbfd5f75638018094f6ce73dd0
Marc Salvadore Silitonga <marc.salvadore@ui.ac.id>
2024-03-11 17:42:42 +0700

    Registration Model and Db

--- siRelawan/src/main/java/propensi/proyek/siRelawan/controller/BaseController.java
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/controller/BaseController.java
@@ -12,8 +12,20 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import propensi.proyek.siRelawan.security.jwt.JwtUtils;
+import propensi.proyek.siRelawan.service.UserService;
 
 @Controller
 public class BaseController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @GetMapping("/")
+    public String home(Model model, HttpServletRequest request) {
+        return new String();
+    }
     
 }
