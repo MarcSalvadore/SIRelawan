8443772c1140657e0488e2686d30b33ce95f5417
Aulia Satrio Wijoyo <aulia.satrio11@ui.ac.id>
2024-04-01 02:53:23 +0700

    Feat: Detail Edit Delete Catalog Done

--- siRelawan/src/main/java/propensi/proyek/siRelawan/service/CatalogServiceImpl.java
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/service/CatalogServiceImpl.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Service;
 import propensi.proyek.siRelawan.model.Catalog;
 import propensi.proyek.siRelawan.repository.CatalogDb;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class CatalogServiceImpl implements CatalogService {
@@ -21,5 +23,20 @@ public class CatalogServiceImpl implements CatalogService {
     @Override
     public List<Catalog> getAllCatalog(){
         return catalogDb.findAll();
+    }
+
+    @Override
+    public Catalog getCatalogById(String id) {
+        return catalogDb.getReferenceById(UUID.fromString(id));
+    }
+
+    @Override
+    public Catalog updateCatalog(Catalog catalog) {
+        return catalogDb.save(catalog);
+    }
+
+    @Override
+    public void deleteCatalogById(String id) {
+        catalogDb.deleteById(UUID.fromString(id));
     }
 }
