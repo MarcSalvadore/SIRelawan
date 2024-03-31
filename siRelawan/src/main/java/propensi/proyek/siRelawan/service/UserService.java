c8992e310cb1f2eada605d66b847cd8f8070c97f
Aulia Satrio Wijoyo <aulia.satrio11@ui.ac.id>
2024-04-01 02:49:33 +0700

    "Feat: Add Relawan Statistik"

--- siRelawan/src/main/java/propensi/proyek/siRelawan/service/UserService.java
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/service/UserService.java
@@ -1,9 +1,11 @@
 package propensi.proyek.siRelawan.service;
 
+import java.util.List;
 import java.util.UUID;
 
 import propensi.proyek.siRelawan.dto.request.CreateUserRequestDTO;
 import propensi.proyek.siRelawan.dto.response.CreateUserResponseDTO;
+import propensi.proyek.siRelawan.model.UserModel;
 
 public interface UserService {
     // String getToken(String username, String fullName);
@@ -13,4 +15,6 @@ public interface UserService {
     void createUser(CreateUserRequestDTO userRequestDTO);
     boolean authenticateUser(String username, String password);
     String getUsername(String username);
+
+    List<UserModel> getAllUser();
 }
