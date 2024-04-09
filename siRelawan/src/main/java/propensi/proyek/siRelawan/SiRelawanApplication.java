20485806b29e8ec90b46f1570fd3071e6c753955
Marc Salvadore Silitonga <marc.salvadore@ui.ac.id>
2024-04-09 20:33:50 +0700

    Sprint Backlog 2 Marc

--- siRelawan/src/main/java/propensi/proyek/siRelawan/SiRelawanApplication.java
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/SiRelawanApplication.java
@@ -1,13 +1,86 @@
 package propensi.proyek.siRelawan;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import propensi.proyek.siRelawan.model.EnumRole;
+import propensi.proyek.siRelawan.model.UserModel;
+import propensi.proyek.siRelawan.repository.UserDb;
 
 @SpringBootApplication
-public class SiRelawanApplication {
+@ComponentScan(basePackages = {"propensi.proyek.siRelawan"})
+public class SiRelawanApplication implements CommandLineRunner{
+	@Autowired
+	private UserDb userDb;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SiRelawanApplication.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+        List<UserModel> dummyRelawan = generateRelawan(10);
+        List<UserModel> dummyAdmin = generateAdmin(2);
+        userDb.saveAll(dummyRelawan);
+        userDb.saveAll(dummyAdmin);
+    }
+
+	public List<UserModel> generateRelawan(int count) {
+        List<UserModel> users = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            String fullName = "Relawan Dummy " + i;
+            String username = "Relawan" + i;
+            String password = "relawan" + i;
+            String email = "relawan" + i + "@dummy.com";
+            Long nomorWA = 628000000000L + random.nextInt(1000000000);
+            EnumRole role = EnumRole.RELAWAN;
+            int poin = 20000 + random.nextInt(150001);
+            Long nik = 1000000000000L + random.nextInt(1000000000);
+            Long npwp = 1000000000000L + random.nextInt(1000000000);
+            Long noRekening = 1000000000000L + random.nextInt(1000000000);
+
+            UserModel user = new UserModel(UUID.randomUUID(), fullName, username, password, email,
+                    nomorWA, role, false, poin, nik, npwp, noRekening);
+            // UserModel user = new UserModel(UUID.randomUUID(), fullName, username, password, email,
+            //         nomorWA, role, false, poin);
+            users.add(user);
+        }
+
+        return users;
+    }
+
+    public List<UserModel> generateAdmin(int count) {
+        List<UserModel> users = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < count; i++) {
+            String fullName = "Admin Salam Setara " + i;
+            String username = "Pak Admin" + i;
+            String password = "admin" + i;
+            String email = "admin" + i + "@sirelawan.com";
+            Long nomorWA = 628000000000L + random.nextInt(1000000000);
+            EnumRole role = EnumRole.SUPERADMIN;
+            Long nik = 1000000000000L + random.nextInt(1000000000);
+            Long npwp = 1000000000000L + random.nextInt(1000000000);
+            Long noRekening = 1000000000000L + random.nextInt(1000000000);
+
+            UserModel user = new UserModel(UUID.randomUUID(), fullName, username, password, email,
+                    nomorWA, role, false, 0, nik, npwp, noRekening);
+            // UserModel user = new UserModel(UUID.randomUUID(), fullName, username, password, email,
+            //         nomorWA, role, false, 0);
+            users.add(user);
+        }
+
+        return users;
+    }
 }
--- siRelawan/src/main/java/propensi/proyek/siRelawan/controller/UserController.java
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/controller/UserController.java
@@ -7,18 +7,17 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import propensi.proyek.siRelawan.dto.request.CreateUserRequestDTO;
 import propensi.proyek.siRelawan.dto.request.LoginUserRequestDTO;
+import propensi.proyek.siRelawan.dto.request.UpdateUserRequestDTO;
 import propensi.proyek.siRelawan.model.UserModel;
-import propensi.proyek.siRelawan.repository.UserDb;
-import propensi.proyek.siRelawan.security.jwt.JwtUtils;
 import propensi.proyek.siRelawan.service.UserService;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -65,6 +64,7 @@ public class UserController {
     @PostMapping("/login")
     public String login(@ModelAttribute("loginRequest") @Valid LoginUserRequestDTO loginRequest,
                         BindingResult bindingResult,
+                        HttpServletRequest request,
                         RedirectAttributes redirectAttributes, Model model) {
         if (bindingResult.hasErrors()) {
             return "login";
@@ -72,12 +72,52 @@ public class UserController {
         boolean isAuthenticated = userService.authenticateUser(loginRequest.getUsername(), loginRequest.getPassword());
         if (isAuthenticated) {
             // Redirect to home.html if isAuthenticated return True
+            HttpSession session = request.getSession();
+            // Store current user in session
+            session.setAttribute("currentUser", loginRequest.getUsername());
+            redirectAttributes.addFlashAttribute("success", "Login Successful!");
             return "redirect:/home";
         } else {
             // Add error message and redirect back to login page
             redirectAttributes.addFlashAttribute("error", "Incorrect username or password");
-            return "redirect:/login";
+            return "user/login";
         }
     }
     
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpSession session, RedirectAttributes redirectAttributes) {
+        // Invalidate the session to delete associated user at previous session
+        session.invalidate();
+        
+        // Redirect to login page with a success message
+        redirectAttributes.addFlashAttribute("success", "You have been logged out successfully.");
+        return "redirect:/login";
+    }
+
+    @GetMapping("user/update")
+    public String formUpdate(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+        String currentUsername = (String) session.getAttribute("currentUser");
+        UserModel currentUserData = userService.getCurrentUser(currentUsername);
+        model.addAttribute("accountData", currentUserData);
+        return "profileAccount";
+    }
+
+    @PostMapping("user/update")
+    public String updateAccount(@Valid @RequestBody UpdateUserRequestDTO updateUserRequestDTO, 
+                                HttpServletRequest request, 
+                                RedirectAttributes redirectAttributes, 
+                                Model model) {
+        HttpSession session = request.getSession();
+        String currentUsername = (String) session.getAttribute("currentUser");
+        // Update user using UpdateUserRequestDTO
+        userService.updateUser(currentUsername, updateUserRequestDTO);
+        UserModel currentUserData = userService.getCurrentUser(currentUsername);
+
+        // Add the updated user data to the model
+        model.addAttribute("accountData", currentUserData);
+        redirectAttributes.addFlashAttribute("success", "Account Updated!");
+        // Return the view name
+        return "profileAccount";
+    }
 }
