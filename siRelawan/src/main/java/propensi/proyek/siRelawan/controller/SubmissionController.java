6b3c9fb07ccd4f1e304394b8149d0c3fae5111ce
m.faiq <m.faiq@ui.ac.id>
2024-05-06 19:11:37 +0700

    feature submission penawaran

---
+++ siRelawan/src/main/java/propensi/proyek/siRelawan/controller/SubmissionController.java
@@ -0,0 +1,21 @@
+package propensi.proyek.siRelawan.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+
+@Controller
+public class SubmissionController {
+}
